cf1a4fc51e21acc73490ac80f803795c81943275
heroccccc <heroccc@gmail.com>
2021-02-08 00:08:40 +0900

    change result page and answer page.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from flask import request, redirect, url_for, render_template
-from main import app
+from main import app, db
 from main.models import User
 
 @app.route('/')
@@ -8,10 +8,23 @@ def main():
 
 @app.route('/', methods=["POST"])
 def import_result():
+    user_name = request.form.get('user_name')
     q1_result = request.form.get('q1_radio')
     q2_result = request.form.get('q2_radio')
     q3_result = request.form.get('q3_radio')
-    print(q1_result, q2_result, q3_result)
+
+    #1つでも入力されていない場合は、dbへinputさせない
+    #この書き方は良くない
+    if not (not user_name or q1_result == None or q2_result == None or q3_result == None):
+        new_result = User(
+                name = user_name,
+                q1_result = q1_result,
+                q2_result = q2_result,
+                q3_result = q3_result
+        )
+        db.session.add(new_result)
+        db.session.commit()
+
     #再読み込み等による、多重投稿防止が必要
     return redirect(url_for('main'))
 
@@ -23,5 +36,5 @@ def answer_question():
 #回答ページへ遷移
 @app.route('/result', methods=["GET"])
 def show_result():
-    data = User.query.all()[0]
-    return render_template("result.html", id=data.id, name=data.name, q1_result=data.q1_result, q2_result=data.q2_result, q3_result=data.q3_result)
+    result_data = User.query.all()
+    return render_template("result.html", result_data=result_data)
